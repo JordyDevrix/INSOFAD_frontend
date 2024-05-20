ea79ea59ffe5f8648dddadd6f152ac381f7391fd
Jordy Devrix <2109jordy2241@gmail.com>
2024-05-20 23:01:47 +0200

    Added the base of the feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
+import { ProductDetailComponent } from "./products/product-detail/product-detail.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent},
+  { path: 'products/:id', component: ProductDetailComponent },
   { path: 'cart', component: CartComponent },
   {path: 'orders', component: OrderHistoryComponent},
   {path: 'account/login', component: LoginComponent },
---
+++ src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,74 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
+import { Category } from '../../models/category.model';
+import { AuthService } from '../../auth/auth.service';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.scss'
+})
+export class ProductDetailComponent implements OnInit{
+  @Output() public onBuyProduct: EventEmitter<Product> = new EventEmitter<Product>();
+  @Input() public product!: Product;
+  //public product: Product;
+  public category: Category;
+  private productId: number;
+  public userIsLoggedIn: boolean = false;
+  public text: string = 'Add to cart';
+  public clickedStyle: string = '#FFF';
+  public showPlus: boolean = true;
+  public productProperties: any;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productsService: ProductsService,
+    private authService: AuthService,
+    private cartService: CartService
+  ) { }
+
+  ngOnInit() {
+    this.checkLoginState();
+
+    this.activatedRoute.params.subscribe(params => {
+      this.productId = params['id'];
+    });
+
+    this.productsService
+      .getProductByIndex(this.productId)
+      .subscribe((product: Product) => {
+        this.product = product;
+        this.productProperties = product.productProperties;
+      });
+  }
+
+  public buyProduct(product: Product) {
+    console.log(product);
+    console.log("Product gekocht")
+    this.cartService.addProductToCart(product)
+    this.text = 'Added to cart';
+    this.clickedStyle = '#0C7';
+    this.showPlus = false;
+    setTimeout(() => {
+      this.text = 'Add to cart';
+      this.clickedStyle = '#FFF';
+      this.showPlus = true;
+    } , 1300);
+  }
+
+  public checkLoginState(): void{
+
+    this.authService
+      .$userIsLoggedIn
+      .subscribe((loginState: boolean) => {
+        this.userIsLoggedIn = loginState;
+      });
+  }
+
+}

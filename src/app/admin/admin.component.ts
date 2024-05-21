959649095e575e545ea0a8e10af58f2909ca5226
Jordy Devrix <2109jordy2241@gmail.com>
2024-05-22 00:33:44 +0200

    completely finished remove product function in frontend

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,54 +3,58 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 import {ProductsService} from "../services/products.service";
 import {Product} from "../models/product.model";
 import {CurrencyPipe} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-admin',
-  standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    CurrencyPipe
-  ],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+    selector: 'app-admin',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        CurrencyPipe
+    ],
+    templateUrl: './admin.component.html',
+    styleUrl: './admin.component.scss'
 })
 export class AdminComponent implements OnInit {
-  public products: Product[] = [];
-  public addProduct: FormGroup;
+    public products: Product[] = [];
+    public addProduct: FormGroup;
 
     constructor(
         private productsService: ProductsService,
-        private fb: FormBuilder
-    ) { }
-
-    ngOnInit(): void {
-      this.productsService.getProducts().subscribe((products) => {
-        this.products = products;
-      });
-      this.addProduct = this.fb.group({
-        name: [''],
-        brand: [''],
-        price: [''],
-        description: [''],
-        size: [''],
-        material: [''],
-        color: [''],
-        image: [''],
-        category: ['']
-      });
+        private fb: FormBuilder,
+        private router: Router
+    ) {
     }
 
-  onRemoveProduct(id: number) {
-    this.productsService.removeProduct(id).subscribe(() => {
-      this.products = this.products.filter(product => product.id !== id);
-    });
-  }
+    ngOnInit(): void {
+        this.productsService.getProducts().subscribe((products) => {
+            this.products = products;
+        });
+        this.addProduct = this.fb.group({
+            name: [''],
+            brand: [''],
+            description: [''],
+            image: [''],
+            category: ['']
+        });
+    }
 
-  onAddProduct() {
+    onRemoveProduct(id: number) {
+        this.productsService.removeProduct(id).subscribe(() => {
+            this.products = this.products.filter(product => product.id !== id);
+        });
+        window.location.reload();
+    }
 
-  }
+    onAddProduct() {
 
-  onRemoveVariant(id: number) {
+    }
 
-  }
+    onRemoveVariant(id: number) {
+        this.productsService.removeVariant(id).subscribe(() => {
+            this.products = this.products.filter(product => product.id !== id);
+            console.log(id)
+        });
+        window.location.reload();
+    }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,36 +1,41 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 
-import { environment } from '../../environments/environment';
-import { Product } from '../models/product.model';
+import {environment} from '../../environments/environment';
+import {Product} from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductsService {
-  private baseUrl: string = environment.base_url + "/products";
+    private baseUrl: string = environment.base_url + "/products";
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  public getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl);
-  }
+    public getProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.baseUrl);
+    }
 
-  public addProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
-  }
+    public addProduct(product: Product): Observable<Product> {
+        return this.http.post<Product>(this.baseUrl, product);
+    }
 
-  public getProductByIndex(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.baseUrl}/${id}`);
-  }
+    public getProductByIndex(id: number): Observable<Product> {
+        return this.http.get<Product>(`${this.baseUrl}/${id}`);
+    }
 
-  public updateProductByIndex(id: number, product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.baseUrl}/${id}`, product);
-  }
+    public updateProductByIndex(id: number, product: Product): Observable<Product> {
+        return this.http.put<Product>(`${this.baseUrl}/${id}`, product);
+    }
 
     public removeProduct(id: number): Observable<void> {
         return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    }
+
+    public removeVariant(id: number) {
+        return this.http.delete<void>(`${this.baseUrl}/variant/${id}`);
     }
 }
